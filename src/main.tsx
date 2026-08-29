6617fb6dfeae23bf045fdabc75d831930f1bc259
jeretuyasix-hub <jeretuyasix@gmail.com>
2026-08-28 22:38:48 -0500

    Desactivar curl experimental para recuperar carga estable

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './lib/pageGesturesV15'
 import './styles.css'
 import './mobile-fixes.css'
 import './mobile-v4.css'
